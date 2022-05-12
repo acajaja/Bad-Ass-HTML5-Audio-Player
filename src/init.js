72abcca5c28bedb152eb4c362167891cbf92e344
Clif Jackson <code@clifjackson.net>
2022-05-12 01:01:46 -0400

    Removed src dir & put evrything in public/js/WebAudioPlayer

--- src/init.js
+++
@@ -1,8 +0,0 @@
-import { init } from './WebAudioPlayer.js';
-
-const la = (e) => {
-    const playerNode = document.getElementById('my-boom-box');
-    init(playerNode);
-}
-
-window.document.addEventListener('DOMContentLoaded', la);
